d8829dc6bbac533248f206187b0ef8f9f2dc773f
Timur Khakkiev <khakkievtimur@yandex.ru>
2021-07-19 11:28:47 +0300

    Исправлен адаптив слайдера. Добавлены картинки слайдов

--- src/Components/Homepage/PromoBlock/PromoBlock.js
+++ src/Components/Homepage/PromoBlock/PromoBlock.js
@@ -23,15 +23,15 @@ const PromoBlock = () => {
 						<div className="promo-block-top">
 							<h2 className="promo-block-top__title">Акции, скидки</h2>
 
-							<div className="promo-block-top__pagination-block pagination">
+							<div className="promo-block-top__pagination-block">
 
-								<button className="pagination__btn-prev">
+								<button className="promo-block-top__btn-prev">
 									<svg className="pagination__icon-prev" width="40" height="40">
 										<use href="icons.svg#prev"></use>
 									</svg>
 								</button>
 
-								<button className="pagination__btn-next">
+								<button className="promo-block-top__btn-next">
 									<svg className="pagination__icon-next" width="40" height="40">
 										<use href="icons.svg#next"></use>
 									</svg>
@@ -39,14 +39,11 @@ const PromoBlock = () => {
 
 							</div>
 						</div>
-
-
 					</div>
 				</Container>
 
 			</div>
 			<Swiper
-				className="promo-block-slider__wrapper"
 				slidesPerView={"auto"}
 				grabCursor={true}
 				loop={true}
@@ -62,27 +59,31 @@ const PromoBlock = () => {
 					}
 				}}
 				navigation={{
-					nextEl: '.pagination__btn-next',
-					prevEl: '.pagination__btn-prev',
+					nextEl: '.promo-block-top__btn-next',
+					prevEl: '.promo-block-top__btn-prev',
 				}}
 			>
 				<SwiperSlide className="promo-block-slider__item green">
 					<h3 className="promo-block-slider__item-title">5 ДОСТАВОК<br/>БЕСПЛАТНО</h3>
-					<span className="promo-block-slider__item-value">4F5B</span>
+					<img className="promo-block-slider__item-img" src="images/promo-img-1.png" alt="овощи" srcset="" />
+					<span className="promo-block-slider__item-value promo-code">4F5B</span>
 				</SwiperSlide>
 
 				<SwiperSlide className="promo-block-slider__item red">
 					<h3 className="promo-block-slider__item-title">СКИДКА<br/>на любые ягоды</h3>
+					<img className="promo-block-slider__item-img" src="images/promo-img-2.png" alt="ягоды" srcset="" />
 					<span className="promo-block-slider__item-value">20%</span>
 				</SwiperSlide>
 
 				<SwiperSlide className="promo-block-slider__item yellow">
 					<h3 className="promo-block-slider__item-title">СКИДКА<br/>на любой мёд</h3>
+					<img className="promo-block-slider__item-img" src="images/promo-img-3.png" alt="банка с медом" srcset="" />
 					<span className="promo-block-slider__item-value">50%</span>
 				</SwiperSlide>
 
 				<SwiperSlide className="promo-block-slider__item blue">
 					<h3 className="promo-block-slider__item-title">СКИДКА<br/>на молочные продукты</h3>
+					<img className="promo-block-slider__item-img" src="images/promo-img-4.png" alt="бутылка молока" srcset="" />
 					<span className="promo-block-slider__item-value">10%</span>
 				</SwiperSlide>
 
