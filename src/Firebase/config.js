655a0b7765c6c7c1c9afda025ea79ebe40f20adf
Timur Khakkiev <khakkievtimur@yandex.ru>
2021-07-25 11:14:51 +0300

    Небольшие изменения

---
+++ src/Firebase/config.js
@@ -0,0 +1,10 @@
+const config = {
+	apiKey: 'AIzaSyBQaJY878XMgqk8LA0Z87D9tTe2j9xtYaQ',
+	authDomain: 'nasha-ferma.firebaseapp.com',
+	projectId: 'nasha-ferma',
+	storageBucket: 'nasha-ferma.appspot.com',
+	messagingSenderId: '811210513196',
+	appId: '1:811210513196:web:598a5823230024985dea89',
+};
+
+export default config;
